0d4c23e0284fbdd33c708ea88ad23079e2307c76
s-ras <rasolisepehr@gmail.com>
2023-07-22 20:23:30 +0330

    wrapped App component in CssBaseline component from mui for consistancy in styling.

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { Fragment } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { CssBaseline } from "@mui/material";
 
 const root = ReactDOM.createRoot(
 	document.getElementById("root") as HTMLElement
 );
 root.render(
 	<React.StrictMode>
-		<App />
+		<Fragment>
+			<CssBaseline>
+				<App />
+			</CssBaseline>
+		</Fragment>
 	</React.StrictMode>
 );
 reportWebVitals();
